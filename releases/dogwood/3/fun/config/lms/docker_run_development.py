3f78f48127ba2492e54bbcf18ba0675fe2cb55ee
Julien Maupetit <julien@maupetit.net>
2019-12-05 16:18:34 +0100

    ⚡️(dogwood/3/fun) fix ORA2 backend settings
    ORA2 production backend should not be FS based. This must only be used
    for development.

--- releases/dogwood/3/fun/config/lms/docker_run_development.py
+++ releases/dogwood/3/fun/config/lms/docker_run_development.py
@@ -23,3 +23,19 @@ STATICFILES_STORAGE = "openedx.core.storage.DevelopmentStorage"
 ALLOWED_HOSTS = ["*"]
 
 FEATURES["AUTOMATIC_AUTH_FOR_TESTING"] = True
+
+# ORA2 fileupload
+ORA2_FILEUPLOAD_BACKEND = "filesystem"
+ORA2_FILEUPLOAD_ROOT = os.path.join(SHARED_ROOT, "openassessment_submissions")
+ORA2_FILEUPLOAD_CACHE_ROOT = os.path.join(
+    SHARED_ROOT, "openassessment_submissions_cache"
+)
+
+
+def ensure_directory_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+ensure_directory_exists(ORA2_FILEUPLOAD_ROOT)
+ensure_directory_exists(ORA2_FILEUPLOAD_CACHE_ROOT)
