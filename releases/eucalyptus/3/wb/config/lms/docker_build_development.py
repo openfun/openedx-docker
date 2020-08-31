7f44edb481d4025234dca80d2099267bfd19628a
Matthieu Huguet <matthieu.huguet@fun-mooc.fr>
2020-08-31 18:17:24 +0200

    🐛(eucalyptus/3/wb) fix redis-sentinel support
    It was impossible to run `make bootstrap` without error with
    `REDIS_SERVICE=redis-sentinel`. To fix that, the settings have been
    adjusted and the `django-redis` version has been pinned to `4.5.0`.

--- releases/eucalyptus/3/wb/config/lms/docker_build_development.py
+++ releases/eucalyptus/3/wb/config/lms/docker_build_development.py
@@ -5,3 +5,35 @@ from .docker_run_development import *
 DATABASES = {"default": {}}
 
 XQUEUE_INTERFACE = {"url": None, "django_auth": None}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "KEY_PREFIX": "default",
+    },
+    "general": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "KEY_PREFIX": "general",
+    },
+    "celery": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "KEY_PREFIX": "celery",
+    },
+    "mongo_metadata_inheritance": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "KEY_PREFIX": "mongo_metadata_inheritance",
+    },
+    "openassessment_submissions": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "KEY_PREFIX": "openassessment_submissions",
+    },
+    "loc_cache": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "KEY_PREFIX": "loc_cache",
+    },
+    # Cache backend used by Django 1.8 storage backend while processing static files
+    "staticfiles": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "KEY_PREFIX": "staticfiles",
+    },
+}
