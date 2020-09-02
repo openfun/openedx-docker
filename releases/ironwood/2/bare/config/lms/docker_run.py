a737739eb6f4b4bd569a1d9f7bc95264078ff1e6
Simon Landrault <simon.landrault@fun-mooc.fr>
2020-09-02 10:01:12 +0200

    🎉(ironwood/2/bare) start the release of ironwood
    Let's start to implement ironwood in the release process.
    This commit is not to be deployed as some details are yet to be
    adjusted.

---
+++ releases/ironwood/2/bare/config/lms/docker_run.py
@@ -0,0 +1,3 @@
+# This file is meant to import the environment related settings file
+
+from docker_run_production import *
---
+++ releases/ironwood/2/bare/config/lms/docker_run_staging.py
@@ -0,0 +1,14 @@
+# This file includes overrides to build the `staging` environment for the LMS starting from the
+# settings of the `production` environment
+
+from docker_run_production import *
+from .utils import Configuration
+
+# Load custom configuration parameters from yaml files
+config = Configuration(os.path.dirname(__file__))
+
+LOGGING["handlers"]["sentry"]["environment"] = "staging"
+
+EMAIL_BACKEND = config(
+    "EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
+)
